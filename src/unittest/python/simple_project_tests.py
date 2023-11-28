0386a8faa3ee45446c49a43010c3ec67f11f223e
raghavharness <raghav.gupta@harness.io>
2023-11-28 12:57:12 +0530

    pybuilder fail test

--- src/unittest/python/simple_project_tests.py
+++ src/unittest/python/simple_project_tests.py
@@ -6,6 +6,10 @@ class TestSimpleProject(unittest.TestCase):
     def test_add_numbers(self):
         result = add_numbers(2, 3)
         self.assertEqual(result, 5)
+    
+    def test_add_numbers_2(self):
+        result = add_numbers(2, 3)
+        self.assertEqual(result, 6)
 
 if __name__ == '__main__':
     unittest.main()
