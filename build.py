190022f4ea72fa6d18e90bb405ba31ce3904a7b3
raghavharness <raghav.gupta@harness.io>
2023-11-28 14:00:45 +0530

    add coverage

--- build.py
+++ build.py
@@ -1,7 +1,15 @@
-from pybuilder.core import use_plugin
+from pybuilder.core import init, use_plugin
 
 use_plugin("python.core")
 use_plugin("python.unittest")
+use_plugin("python.coverage")
 
 default_task = "publish"
 
+@init
+def set_properties(project):
+    project.set_property("coverage_threshold_warn", 80)
+    project.set_property("coverage_break_build", False)
+    project.set_property("coverage_reset_modules", True)
+    project.set_property('coverage_break_build', False)
+    project.set_property("coverage_exceptions", ["__init__"])
--- src/unittest/python/simple_project_tests.py
+++ src/unittest/python/simple_project_tests.py
@@ -9,7 +9,7 @@ class TestSimpleProject(unittest.TestCase):
     
     def test_add_numbers_2(self):
         result = add_numbers(2, 3)
-        self.assertEqual(result, 6)
+        self.assertEqual(result, 5)
 
 if __name__ == '__main__':
     unittest.main()
